4caf8198847bd505ff15d9798e7b948da4b56800
Zimon-Kuhs <zimon.kuhs@axis.com>
2021-12-06 05:49:40 +0100

    Initial 05.

--- src/year2021/december_05.py
+++ src/year2021/december_05.py
@@ -6,11 +6,19 @@
 """
 
 
+from year2021.utility import parse_problem
+
+
 def solve():
     """
         Solves the problem for December 05.
 
         @return the solution to the problem.
     """
+
+    vectors = [(coords[0], coords[1]) for line in parse_problem(__name__) for coords in line.split(" -> ") ]
+
+    for vector in vectors:
+        print(vector)
 
     return "TBI"
